a6bc3bdfbc68ef7b205cf642c73f57eb5c3083df
shivani joshi <161042809+749977@users.noreply.github.com>
2025-03-07 15:28:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector(".viewer");
+const toggle = document.querySelector(".toggle");
+const progress = document.querySelector(".progress");
+const volume = document.querySelector(".volume");
+const playbackSpeed = document.querySelector(".playbackSpeed");
+const rewind = document.querySelector(".rewind");
+const forward = document.querySelector(".forward");
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+        toggle.textContent = "❚ ❚";
+    } else {
+        video.pause();
+        toggle.textContent = "►";
     }
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateProgress() {
+    progress.value = (video.currentTime / video.duration) * 100;
+}
+
+function setProgress() {
+    video.currentTime = (progress.value / 100) * video.duration;
+}
+
+function updateVolume() {
+    video.volume = volume.value;
+}
+
+function updateSpeed() {
+    video.playbackRate = playbackSpeed.value;
+}
+
+function skipBackward() {
+    video.currentTime -= 10;
+}
+
+function skipForward() {
+    video.currentTime += 25;
+}
+
+video.addEventListener("click", togglePlay);
+toggle.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", updateProgress);
+progress.addEventListener("input", setProgress);
+volume.addEventListener("input", updateVolume);
+playbackSpeed.addEventListener("input", updateSpeed);
+rewind.addEventListener("click", skipBackward);
+forward.addEventListener("click", skipForward);
